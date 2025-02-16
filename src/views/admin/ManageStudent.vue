af2fe4823f8b1e364a3cec95d7bcdb403ff59c55
GH0ST-141 <151105998+GH0ST-141@users.noreply.github.com>
2025-02-16 10:00:03 +0800

    UPDATE 7
    - Added template modals for admin files: [classlist.vue, classschedule.vue, manageclass.vue, manageevents.vue, managestudent.vue, manageteacher.vue]
    - Added template modals for teacher file: [teacherclassschdule.vue]

--- src/views/admin/ManageStudent.vue
+++ src/views/admin/ManageStudent.vue
@@ -10,14 +10,14 @@
         </div>
 
         <section class="content-section">
-          
+
           <div class="controls">
             <div class="search-bar">
               <input type="text" v-model="searchQuery" placeholder="Search Students" />
               <i class="fas fa-search"></i>
             </div>
 
-            
+
             <div class="filter">
               <select v-model="selectedCourse" @change="filterStudents">
                 <option value="">All Courses</option>
@@ -41,11 +41,8 @@
                 </tr>
               </thead>
               <tbody>
-                <tr 
-                  v-for="(student, index) in filteredStudents" 
-                  :key="index"
-                  :class="{ 'row-light': index % 2 === 0, 'row-dark': index % 2 !== 0 }"
-                >
+                <tr v-for="(student, index) in filteredStudents" :key="index"
+                  :class="{ 'row-light': index % 2 === 0, 'row-dark': index % 2 !== 0 }">
                   <td>{{ student.studentId }}</td>
                   <td>{{ student.firstName }}</td>
                   <td>{{ student.lastName }}</td>
@@ -53,10 +50,10 @@
                   <td>{{ student.year }}</td>
                   <td>{{ student.course }}</td>
                   <td>
-                    <button class="edit-button">
+                    <button @click="openModal('edit')" class="edit-button">
                       <i class="fas fa-edit"></i>
                     </button>
-                    <button class="delete-button">
+                    <button @click="openModal('delete')" class="delete-button">
                       <i class="fas fa-trash-alt"></i>
                     </button>
                   </td>
@@ -68,6 +65,46 @@
       </div>
     </div>
   </div>
+
+  <!-- Modal -->
+  <!-- start of modal -->
+  <div>
+    <div v-if="isOpen" class="modal-overlay">
+      <div class="modal-container">
+        <div class="close-button-container">
+          <button @click="closeModal" class="fa-regular fa-circle-xmark modal-close-button "></button>
+        </div>
+
+        <!-- edit student details -->
+        <div v-if="modalType === 'edit'">
+          <h2 class="modal-title">Edit Student</h2>
+          <div class="modal-form-container">
+            <div class="form-input">
+              <input id="SampleID1" type="text" placeholder="Sample Label Template 1" />
+            </div>
+            <div class="form-input">
+              <input id="SampleID2" type="text" placeholder="Sample Label Template 2" />
+            </div>
+            <div class="form-input">
+              <input id="SampleID3" type="text" placeholder="Sample Label Template 3" />
+            </div>
+          </div>
+          <button class="modal-submit-button">SAVE</button>
+        </div>
+
+        <!-- Delete Student -->
+        <div v-if="modalType === 'delete'">
+          <h2 class="modal-title">Delete Student</h2>
+          <div style="justify-content: right; display: flex; column-gap: 20px;">
+            <button class="modal-submit-button">CONFIRM</button>
+            <button class="modal-cancel-button">CANCEL</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <!-- end of modal -->
 </template>
 
 <script>
@@ -83,6 +120,8 @@ export default {
   name: 'ManageStudents',
   data() {
     return {
+      isOpen: false, //for modal
+      modalType: '',
       searchQuery: '',
       selectedCourse: '',
       students: [
@@ -104,8 +143,8 @@ export default {
       if (this.searchQuery) {
         filteredStudents = filteredStudents.filter((student) => {
           return student.firstName.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-                 student.lastName.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-                 student.middleName.toLowerCase().includes(this.searchQuery.toLowerCase());
+            student.lastName.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+            student.middleName.toLowerCase().includes(this.searchQuery.toLowerCase());
         });
       }
 
@@ -120,12 +159,112 @@ export default {
   },
   methods: {
     goToRegisterStudent() {
-  this.$router.push('/admin/register-student');
-},
+      this.$router.push('/admin/register-student');
+    },
+    openModal(type) {
+      this.modalType = type;
+      this.isOpen = true; //for modal
+    },
+    closeModal() {
+      this.isOpen = false; //for modal
+    },
 
   },
 };
 </script>
 <style scoped>
+/* for modal [START]*/
+.modal-overlay {
+  position: fixed;
+  inset: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+  background-color: rgba(107, 114, 128, 0.5);
+  /* Tailwind's bg-gray-500 with opacity */
+  padding: 1rem;
+}
 
+.modal-container {
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  justify-content: center;
+  padding: 1.5rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  max-width: 32rem;
+  /* Equivalent to max-w-lg */
+}
+
+.close-button-container {
+  display: flex;
+  justify-content: right;
+}
+
+.modal-close-button {
+  background-color: transparent;
+  border: none;
+  background: linear-gradient(to bottom, #d02e1c 23%, #791e1e 100%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  border-radius: 100%;
+  cursor: pointer;
+  font-size: 1.5rem;
+}
+
+.modal-title {
+  font-size: 1.25rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  background: linear-gradient(to bottom, #d02e1c 23%, #791e1e 100%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  text-align: center;
+}
+
+.modal-form-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-bottom: 1rem;
+}
+
+.form-input {
+  position: flex;
+  width: 100%;
+  border: none;
+}
+
+.form-input input {
+  padding: 10px 30px 10px 30px;
+  width: 100%;
+  border: none;
+  border-radius: 10px;
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.3);
+  background-color: #f7f8ff;
+  box-sizing: border-box;
+}
+
+.modal-submit-button {
+  background: linear-gradient(180deg, rgba(208, 46, 28, 1) 0%, rgba(121, 30, 30, 1) 100%);
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+.modal-cancel-button {
+  background: linear-gradient(180deg, rgba(208, 46, 28, 1) 0%, rgba(121, 30, 30, 1) 100%);
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+/* for modal [END]*/
 </style>
